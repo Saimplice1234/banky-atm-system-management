91d7f8de442bcecee6ac3d729c6143fd69113ef3
Saimplice Ruud Christopher <saimpliceudemy@gmail.com>
2022-07-22 12:40:32 -0400

    Removed

--- README.md
+++
@@ -1,2 +0,0 @@
-# Bank-System-Management
-It's a bank system manager designed with C and the assembly x64 language.
--- lib.asm
+++
@@ -1,9 +0,0 @@
-BITS 64
-section.text
-  global _start
-  _start:
-    jmp _exit_SYS
-  _exit_SYS:
-    mov rax,60
-    mov rdi,0
-    syscall
